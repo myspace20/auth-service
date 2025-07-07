3845906d15ead3792cacf0073d9ebbe4d3025427
rogersatsi <rogersatsi98@gmail.com>
2025-07-07 01:03:05 +0000

    feat: add repositories for user roles and user models

---
+++ src/main/java/com/bytebites/auth_service/infrastructure/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.bytebites.auth_service.infrastructure;
+
+import com.bytebites.auth_service.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
---
+++ src/main/java/com/bytebites/auth_service/infrastructure/UserRepository.java
@@ -0,0 +1,13 @@
+package com.bytebites.auth_service.infrastructure;
+
+import com.bytebites.auth_service.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+}
