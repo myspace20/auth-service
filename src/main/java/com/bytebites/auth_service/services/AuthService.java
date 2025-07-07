cefa3403407f0e138ee194c3f358a96a6f4261da
rogersatsi <rogersatsi98@gmail.com>
2025-07-07 05:53:09 +0000

    chore: add user response import

--- src/main/java/com/bytebites/auth_service/services/AuthService.java
+++ src/main/java/com/bytebites/auth_service/services/AuthService.java
@@ -3,7 +3,7 @@ package com.bytebites.auth_service.services;
 import com.bytebites.auth_service.dto.LoginRequest;
 import com.bytebites.auth_service.dto.RegisterRequest;
 import com.bytebites.auth_service.dto.AuthResponse;
-import com.bytebites.auth_service.models.User;
+import com.bytebites.auth_service.dto.UserResponse;
 
 public interface AuthService {
     UserResponse register(RegisterRequest registerRequest);
